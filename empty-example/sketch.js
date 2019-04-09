d005bfd20dda6bad0b51ee3b2c340a2cad76d563
Maciej Dąbkowski <m.dabkowski@stud.elka.pw.edu.pl>
2019-04-09 16:05:29 +0200

    Podstawy obiektow gry, rysowanie ich

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,16 +1,53 @@
+var enemy;
+var crosshair;
+var windowSizeX;
+var windowSizeY;
 function setup() {
-  // put setup code here
+  windowSizeX=1280;
+  windowSizeY=720;
+  enemy = new Enemy;
+  crosshair = new Crosshair
+  createCanvas(windowSizeX,windowSizeY);
+  noCursor();
 }
 
 function draw() {
   background(237, 34, 93);
-  fill(0);
+  enemy.show();
+  crosshair.show();
+  crosshair.updateMouse();
+}
+function Crosshair(){
+  this.x = mouseX;
+  this.y = mouseY;
 
-  if (mouseIsPressed) {
-    ellipse(50, 50, 50, 50);
-  } else {
-    rect(25, 25, 50, 50);
+  this.show=function(){
+    fill(0);
+    ellipse(this.x, this.y, 10, 10);
   }
 
-  print(mouseIsPressed);
+  this.updateMouse=function(){
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+}
+function Enemy(){
+  console.log(windowSizeX);
+  console.log(windowSizeY);
+  this.x = Math.random()*windowSizeX;
+  this.y = Math.random()*windowSizeY;
+  this.r = 50;
+
+  this.show = function(){
+    fill(255);
+    ellipse(this.x, this.y, 2 * this.r, 2 * this.r);
+  }
+  this.changePosition=function(){
+    this.x = Math.random()*windowSizeX;
+    this.y = Math.random()*windowSizeY;
+  }
+  this.checkCollision=function(){
+
+  }
+
 }
